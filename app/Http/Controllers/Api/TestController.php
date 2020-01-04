c89734629c9a0db5fbfb797f7efe621fc675450e
jiuli2001 <git@github.com>
2020-01-04 08:34:56 +0800

    123

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -81,24 +81,8 @@ class TestController extends Controller
      * 获取用户列表
      * 2020年1月2日16:32:07
      */
-    public function userList()
-    {
-        $user_token = $_SERVER['HTTP_TOKEN'];
-        echo 'user_token: '.$user_token;echo '</br>';
-        $current_url = $_SERVER['REQUEST_URI'];
-        echo "当前URL: ".$current_url;echo '<hr>';
-        //echo '<pre>';print_r($_SERVER);echo '</pre>';
-        //$url = $_SERVER[''] . $_SERVER[''];
-        $redis_key = 'str:count:u:'.$user_token.':url:'.md5($current_url);
-        echo 'redis key: '.$redis_key;echo '</br>';
-        $count = Redis::get($redis_key);        //获取接口的访问次数
-        echo "接口的访问次数： ".$count;echo '</br>';
-        if($count >= 5){
-            echo "请不要频繁访问此接口，访问次数已到上限，请稍后再试";
-            Redis::expire($redis_key,3600);
-            die;
-        }
-        $count = Redis::incr($redis_key);
-        echo 'count: '.$count;
+    function userlist(){
+        $list=UserModel::all();
+        echo '<pre>';print_r($list->toArray());echo '</pre>';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,5 @@ Route::get('/api/test','Api\TestController@test');
 
 Route::post('/api/user/reg','Api\TestController@reg');          //用户注册
 Route::post('/api/user/login','Api\TestController@login');      //用户登录
-Route::get('/api/user/list','Api\TestController@userList');      //用户列表
+Route::get('/api/user/list','Api\TestController@userList')->middleware('filter');      //用户列表
 
